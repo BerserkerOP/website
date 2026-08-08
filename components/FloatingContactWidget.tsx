a2c56441f753e4fa2d6f6bebbcf6fdda90a24a0d
BerserkerOP <BerserkerOP@users.noreply.github.com>
2026-08-08 20:15:36 +0530

    Fix floating widget: remove Book a Call text leak when not hovered

--- components/FloatingContactWidget.tsx
+++ components/FloatingContactWidget.tsx
@@ -112,7 +112,7 @@ export default function FloatingContactWidget() {
                 <path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10zm0-12H5V6h14v2z"/>
               </svg>
 
-              {isWidgetHovered ? (
+              {isWidgetHovered && (
                 <motion.span 
                   initial={{ opacity: 0, x: -3 }}
                   animate={{ opacity: 1, x: 0 }}
@@ -121,10 +121,6 @@ export default function FloatingContactWidget() {
                 >
                   Book a Call
                 </motion.span>
-              ) : (
-                <span className="hidden whitespace-nowrap font-extrabold text-xs tracking-tight relative z-10 sm:inline-block">
-                  Book a Call
-                </span>
               )}
             </motion.button>
 
