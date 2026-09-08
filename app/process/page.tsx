589347202dad69a73ebd427c64be7ac7673d2c06
Atharv <77949978+BerserkerOP@users.noreply.github.com>
2026-09-08 19:05:36 +0530

    Redesign portfolio around motion-led editorial identity

--- app/process/page.tsx
+++ app/process/page.tsx
@@ -1,16 +1,2 @@
-"use client";
-
-import HowItWorks from '@/components/HowItWorks';
-import FadeIn from '@/components/FadeIn';
-
-export default function ProcessPage() {
-  return (
-    <div className="bg-apple-bg min-h-screen w-full pt-8 relative overflow-hidden flex flex-col">
-      <div className="relative z-10 w-full max-w-7xl mx-auto px-6 lg:px-16 flex-grow flex flex-col items-center justify-center pt-20">
-        <div className="w-full">
-          <HowItWorks />
-        </div>
-      </div>
-    </div>
-  );
-}
+import {Process,FAQ} from '@/components/Studio';
+export default function Page(){return <div className="inner-page"><Process/><FAQ/></div>}
